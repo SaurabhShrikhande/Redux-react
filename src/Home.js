5d344de6f5354c4f1aa52e58f4ca8d57e45100f8
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-02-02 01:50:23 +0530

    100vh home

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,13 @@ export default function Home(){
     return (<div>
        <div style={{display:"flex", flexDirection:"column", minHeight:"100vh", backgroundColor:"#04152D"}}>
           <Nav/>
+          <div style={{color:"white", minHeight:"80vh" , display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+          <h1 style={{fontSize:"80px", margin:"0"}}>Welcome.</h1>
+          <h3>Millions of movies, TV shows and people to discover. Explore now.</h3>
           <div>
+          <input type="text" placeholder="Search for movie or TV Show...." style={{height:"40px", width:"40vw", borderRadius:"20px 0 0 20px", padding:"10px", fontSize:"20px"}}/>
+          <button style={{height:"64px", width:"12vw", borderRadius:"0 20px 20px 0", padding:"10px", backgroundColor:"orange", color:"white", fontSize:"20px"}}>Search</button>
+          </div>
 
           </div>
        </div>
