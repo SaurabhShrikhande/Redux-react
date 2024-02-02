95be760841e337d863218c437ff66ed660b9f37a
Saurabh Shrikhande <saurabhshrikhande200@gmail.com>
2024-02-02 11:57:10 +0530

    router initialise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Home from './Home';
-import Nav from './Nav';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
 
 function App() {
+  const router =  createBrowserRouter([
+    {
+      path : "/",
+      element : <Home/>
+    },
+    {
+      path : "movies",
+      element : <h1>Movies</h1>
+    },
+    {
+      path : "tvshows",
+      element : <h1>TV Shows</h1>
+    }
+  ])
+
   return (
     <div className="App">
-       <Home/>
+      <RouterProvider router ={router} />
+       
        
 
     </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 export default function Nav(){
 
     return (
@@ -5,8 +6,9 @@ export default function Nav(){
    <div style={{display:"flex", backgroundColor:"rgb(3,16,34, 0.5)", justifyContent:"space-around"}}>
     <img src="https://moviex-olive.vercel.app/assets/movix-logo-d720c325.svg" alt=""/>
     <div style={{display:"flex", gap:"25px", color:"white"}}>
-        <h3>Movies</h3>
-        <h3>TV Shows</h3>
+     <Link to= "./movies" style={{color:"white", textDecoration:"none"}}><h3>Movies</h3> </Link>
+     <Link to="./tvshows" style={{color:"white", textDecoration:"none"}}><h3>TV Shows</h3></Link>
+        
         <h3>
         <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </h3> 
