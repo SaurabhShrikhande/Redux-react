ed19ad10ab11d948dace5b6c3edbed2c5fe71b70
Saurabh Shrikhande <saurabhshrikhande200@gmail.com>
2024-02-02 14:46:35 +0530

    api call & console using axios

--- src/App.js
+++ src/App.js
@@ -4,10 +4,42 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom"
-import Movies from './Movies';
-import TVShows from './TVShows';
+import { useEffect } from 'react';
+import axios from 'axios';
+  import Movies from './Movies';
+  import TVShows from './TVShows';
+
+
+
 
 function App() {
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+   {
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDJmN2E0OTFjODMwNTJmOGYwYjgxNDQwZWVhNmVhNiIsInN1YiI6IjY1YmI5MmQ4YzE0NGRkMDE3Y2ZlZjhmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cXsnR0cqAOdYRdaCAT3SCQM9xLswfs-MUVcCx8zpvQc'
+    }
+   }
+    )
+    .then((res) => console.log(res))
+    .catch(err => console.log(err))
+  
+    // const options = {
+    //   method: 'GET',
+    //   headers: {
+    //     accept: 'application/json',
+    //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDJmN2E0OTFjODMwNTJmOGYwYjgxNDQwZWVhNmVhNiIsInN1YiI6IjY1YmI5MmQ4YzE0NGRkMDE3Y2ZlZjhmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cXsnR0cqAOdYRdaCAT3SCQM9xLswfs-MUVcCx8zpvQc'
+    //   }
+    // };
+    
+    // fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+    //   .then(response => response.json())
+    //   .then(response => console.log(response))
+    //   .catch(err => console.error(err));
+  }, [])
+  
+
   const router =  createBrowserRouter([
     {
       path : "/",
